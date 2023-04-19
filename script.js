868853e95efcc4791d30ee923d1056cdae0e9613
kmuy1212 <85259641+kmuy1212@users.noreply.github.com>
2023-04-19 04:20:21 -0500

    2 files modified, 1 added

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello!');
+function cambiarEstilos() {
+  var estilos1 = document.getElementById('styles');
+  var estilos2 = document.getElementById('estilo2');
+
+  if (estilos1.getAttribute('href') == 'styles.css') {
+    estilos1.setAttribute('href', 'estilo2.css');
+    estilos2.setAttribute('href', 'styles.css');
+  } else {
+    estilos1.setAttribute('href', 'styles.css');
+    estilos2.setAttribute('href', 'estilo2.css');
+  }
+}
